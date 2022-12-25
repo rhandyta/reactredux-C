b28657af7cdd6cf6d7024c9af73037e3e645961f
Rhandyta Briantama <rhandytabriantama@gmail.com>
2022-12-25 16:34:53 +0700

    react logger | Finish

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,14 @@
 import { combineReducers, createStore } from "redux";
 import glovesReducer from "./gloves/glovesReducer";
 import shoesReducer from "./shoes/shoesReducer";
+import { applyMiddleware } from "redux";
+import logger from "redux-logger";
 
 const rootReducer = combineReducers({
     shoes: shoesReducer,
     gloves: glovesReducer,
 });
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, applyMiddleware(logger));
 
 export default store;
